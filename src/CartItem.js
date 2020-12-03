f007e175de5aeb54301450afe934e7fce83a700a
Subham Yadav <subhamy142@gmail.com>
2020-12-04 00:02:56 +0530

    added icon and onClick function

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,47 @@
 import React from 'react';
 class CartItem extends React.Component{
-    render(){
+    constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'Mobile Phone',
+            qty: 1,
+            img:''
+        }
+    }
+    increaseQuantity = () => {
+        console.log('this',this.state);
+    }
+    render () {
+        const {price, title, qty } = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image} alt="something"/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Something</div>
-                    <div style={{color:'#777'}}>999</div>
-                    <div style={{color:'#777'}}>Quantity : 1</div>
+        <div style={{fontSize:25}}>{title}</div>
+        <div style={{color:'#777'}}>Rs {price}</div>
+        <div style={{color:'#777'}}>Quantity : {qty}</div>
                     <div className="cart-item-actions">
-                        {/* buttons */}
+                        <img
+                        alt="increase"
+                        className="action-icons"
+                        src="https://www.flaticon.com/svg/static/icons/svg/992/992651.svg"
+                        onClick={this.increaseQuantity}
+                        />
+
+                        <img
+                        alt="decrease"
+                        className="action-icons"
+                        src="https://www.flaticon.com/premium-icon/icons/svg/2985/2985073.svg"
+                        />
+
+                        <img
+                        alt="delete"
+                        className="action-icons"
+                        src="https://www.flaticon.com/svg/static/icons/svg/1345/1345823.svg"
+                        />
                     </div>
                 </div>
             </div>
