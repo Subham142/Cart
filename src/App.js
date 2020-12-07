199108ce33d5db298a7956e972bd4fdf841eb306
Subham Yadav <subhamy142@gmail.com>
2020-12-07 18:54:59 +0530

    added database and made it realtime

--- src/App.js
+++ src/App.js
@@ -1,41 +1,35 @@
 import React from 'react';
 import Cart from './Cart';
 import Navbar from './Navbar';
+import firebase from 'firebase/app'
 
 class App extends React.Component {
 
   constructor () {
     super();
     this.state = {
-      products: [
-        {
-          price: 99,
-          title: 'Watch',
-          qty: 1,
-          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
-          id: 1
-        },
-        {
-          price: 999,
-          title: 'Mobile Phone',
-          qty: 10,
-          img: 'https://images.unsplash.com/photo-1520923642038-b4259acecbd7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1306&q=80',
-          id: 2
-        },
-        {
-          price: 999,
-          title: 'Laptop',
-          qty: 4,
-          img: 'https://images.unsplash.com/photo-1504707748692-419802cf939d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1330&q=80',
-          id: 3
-        }
-      ]
+      products: [],
+      loading: true
     }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
   }
+
+  componentDidMount(){
+    firebase
+    .firestore()
+    .collection("products")
+    .onSnapshot(Snapshot => {
+      const products =Snapshot.docs.map(doc => {
+        const data = doc.data();
+        data["id"] = doc.id;
+        return data;
+      });
+      this.setState({products : products,
+      loading : false})
+    });
+  }
+
   handleIncreaseQuantity = (product) => {
-    console.log('Heyy please inc the qty of ', product);
+    
     const { products } = this.state;
     const index = products.indexOf(product);
 
@@ -46,7 +40,7 @@ class App extends React.Component {
     })
   }
   handleDecreaseQuantity = (product) => {
-    console.log('Heyy please inc the qty of ', product);
+    
     const { products } = this.state;
     const index = products.indexOf(product);
 
@@ -88,13 +82,17 @@ class App extends React.Component {
     let cartTotal = 0;
 
     products.map((product) => {
-      cartTotal = cartTotal + product.qty * product.price
-    })
+      if(product.qty>0){
+        cartTotal = cartTotal + product.qty * product.price
+    }
+    return '';
+    });
+      
 
     return cartTotal;
   }
   render () {
-    const { products } = this.state;
+    const { products , loading } = this.state;
     return (
       <div className="App">
         <Navbar count={this.getCartCount()} />
@@ -104,7 +102,9 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
-        <div style={ {padding: 10, fontSize: 20} }>TOTAL: {this.getCartTotal()} </div>
+        { loading && <h1>Loading Products.....</h1>}
+        <div style={ {padding: 10, fontSize: 20} }>
+          TOTAL: {this.getCartTotal()} </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase/app'
+import 'firebase/firestore';
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDdMyVCN6egNLs49BOZcRb7M-4dC1eMNGc",
+  authDomain: "cart-83939.firebaseapp.com",
+  projectId: "cart-83939",
+  storageBucket: "cart-83939.appspot.com",
+  messagingSenderId: "1025987372917",
+  appId: "1:1025987372917:web:feabd5d077c703b8b9153c"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
